3291250a7e362ecf2d1a1530f24ca459061470dd
Mohamed0257 <mohamedalshrif367@gmail.com>
2025-05-10 01:43:56 +0300

    Update desserts.js

--- desserts.js
+++ desserts.js
@@ -1,14 +1,3 @@
-onload = function showTab(tabId) {
-    const tabs = document.querySelectorAll('.tab');
-    const contents = document.querySelectorAll('.content');
-
-    tabs.forEach(tab => tab.classList.remove('active'));
-    contents.forEach(content => content.classList.remove('active'));
-
-    document.querySelector(`.tab[onclick="showTab('${tabId}')"]`).classList.add('active');
-    document.getElementById(tabId).classList.add('active');
-}
-
 let total = 0;
 function addToInvoice(item, price) {
     const list = document.getElementById('invoice-list');
